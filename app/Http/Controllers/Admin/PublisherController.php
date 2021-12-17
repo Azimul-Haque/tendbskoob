e3c4c0f934f435460d7db6403aed43f5bcad7e34
A. H. M. Azimul Haque <orbachinujbuk@gmail.com>
2021-12-18 01:24:50 +0600

    ASD

--- app/Http/Controllers/Admin/PublisherController.php
+++ app/Http/Controllers/Admin/PublisherController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\CPU\BackEndHelper;
+use App\CPU\Helpers;
+use App\CPU\ImageManager;
+use App\Http\Controllers\BaseController;
+use App\Model\Brand;
+use App\Model\Category;
+use App\Model\Color;
+use App\Model\DealOfTheDay;
+use App\Model\FlashDealProduct;
+use App\Model\Product;
+use App\Model\Author;
+use App\Model\Review;
+use App\Model\Translation;
+use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Rap2hpoutre\FastExcel\FastExcel;
+use function App\CPU\translate;
+use App\Imports\AuthorImport;
+use Excel;
+use Image;
 
-class PublisherController extends Controller
+class PublisherController extends BaseController
 {
     //
 }
