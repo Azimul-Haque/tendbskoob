40dc2e577876fbae95035a8f282acc176bcc9c68
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-08 01:38:49 +0600

    Commit Fri 04/08/2022  1:38:49.40 23775

--- resources/views/seller-views/product/add-new.blade.php
+++ resources/views/seller-views/product/add-new.blade.php
@@ -77,7 +77,6 @@
                         <div class="card-body">
                             <div class="form-group">
                                 <label for="publisher_id">{{\App\CPU\translate('Publication')}} *</label>
-                                {{ print_r(auth('seller')) }}
                                 <select
                                     class="js-example-basic-multiple js-states js-example-responsive form-control" name="publisher_id" id="publisher_id" required>
                                     
