b25739963f901bc6e9c3ffc14e8f48d18b5f94ce
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-07 18:58:19 +0600

    Commit 07-Apr-22 18:58:19.37 10196

--- resources/views/admin-views/seller/approve.blade.php
+++ resources/views/admin-views/seller/approve.blade.php
@@ -81,7 +81,7 @@
                                         {{-- <button type="submit" class="btn btn-danger">{{\App\CPU\translate('reject')}}</button> --}}
                                     </form>
                                 @endif
-                            @elseif ($seller->)
+                            @elseif ($seller->s)
                             @endif
                         </div>
                     </div>
