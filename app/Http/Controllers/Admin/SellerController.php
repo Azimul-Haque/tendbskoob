24542900eff284f7c02ff7088dd194a10a3cd608
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-07 11:19:33 +0600

    Commit Thu 04/07/2022 11:19:33.43 2736

--- app/Http/Controllers/Admin/SellerController.php
+++ app/Http/Controllers/Admin/SellerController.php
@@ -181,6 +181,71 @@ class SellerController extends Controller
         return view('admin-views.seller.edit', compact('seller'));
     }
 
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name'             => 'required|max:100',
+            'address'          => 'required|max:150',
+            'image'            => 'required|image|mimes:jpeg,png,jpg,gif,bmp,bmp,tiff|max:200',
+            'description'      => 'required|max:200',
+            'collection_point' => 'required',
+            'payment_number'   => 'required',
+            'payment_option'   => 'required',
+            'email'            => 'required|unique:sellers',
+            'password'         => 'required|confirmed|min:6',
+        ]);
+
+        DB::transaction(function ($r) use ($request) {
+            $seller = new Seller();
+            $seller->name = $request->name;
+            $seller->address = $request->address;
+            $seller->description = $request->description;
+            $seller->collection_point = $request->collection_point;
+            $seller->payment_number = $request->payment_number;
+            $seller->payment_option = $request->payment_option;
+
+            if($request->hasFile('image')) {
+                $image    = $request->file('image');
+                $filename = Helpers::random_slug(10) . '.jpg';
+                $location = public_path('/public/images/publisher/'. $filename);
+                Image::make($image)->fit(200, 200)->save($location);
+                $seller->image = $filename;
+            }
+            $seller->email = $request->email;
+            $seller->password = bcrypt($request->password);
+            $seller->status = "pending";
+            $seller->save();
+
+            // APATOTO NEW SHOP CREATE KORTESI NA
+            // APATOTO NEW SHOP CREATE KORTESI NA
+            // $shop = new Shop();
+            // $shop->seller_id = $seller->id;
+            // $shop->name = $request->shop_name;
+            // $shop->address = $request->shop_address;
+            // $shop->contact = $request->phone;
+            // $shop->image = ImageManager::upload('shop/', 'png', $request->file('logo'));
+            // $shop->banner = ImageManager::upload('shop/banner/', 'png', $request->file('banner'));
+            // $shop->save();
+
+            DB::table('seller_wallets')->insert([
+                'seller_id' => $seller['id'],
+                'withdrawn' => 0,
+                'commission_given' => 0,
+                'total_earning' => 0,
+                'pending_withdraw' => 0,
+                'delivery_charge_earned' => 0,
+                'collected_cash' => 0,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+
+        });
+
+        Toastr::success('Publication applied successfully!');
+        return redirect()->route('seller.auth.login');
+
+    }
+
     public function order_list($seller_id)
     {
         $orders = Order::where('seller_id', $seller_id)->where('seller_is', 'seller');
