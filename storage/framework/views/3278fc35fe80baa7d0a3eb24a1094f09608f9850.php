1ad405f764a3c149f41b3521785d3f8c19ee23c9
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-07 10:31:34 +0600

    Commit Thu 04/07/2022 10:31:34.42 10957

--- storage/framework/views/3278fc35fe80baa7d0a3eb24a1094f09608f9850.php
+++ storage/framework/views/3278fc35fe80baa7d0a3eb24a1094f09608f9850.php
@@ -53,121 +53,97 @@
                                class="tio-edit"></i><?php echo e(\App\CPU\translate('Seller_Edit')); ?></h4>
                     </div>
                     <div class="row">
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-6 for-card col-md-6 mt-4">
-                            s
-                        </div>
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-6 for-card col-md-6 mt-4" style="cursor: pointer">
-                            s
+                        <div class="col-lg-12">
+                            <div class="p-2">
+                                <form class="user mt-4" action="<?php echo e(route('shop.apply')); ?>" method="post" enctype="multipart/form-data">
+                                    <?php echo csrf_field(); ?>
+                                    <h5 class="black">প্রকাশনী সম্পর্কিত তথ্যাদি</h5>
+                                    <div class="form-group row">
+                                        <div class="col-md-4 mb-3 mb-sm-0">
+                                            <label for="name">প্রকাশনীর নাম</label>
+                                            <input type="text" class="form-control form-control-user" id="name" name="name" value="<?php echo e(old('name')); ?>" placeholder="Din Publications" required>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="address">প্রকাশনীর ঠিকানা</label>
+                                            <input type="text" class="form-control form-control-user" id="address" name="address" value="<?php echo e(old('address')); ?>" placeholder="Banglabazar, Dhaka" required>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="address">প্রকাশনীর লোগো <span style="color:red;">(অনুপাত - ১ঃ১)</span></label>
+                                            <div class="form-group">
+                                                <div class="custom-file" style="text-align: left">
+                                                    <input type="file" name="image" id="customFileUpload" class="custom-file-input"
+                                                        accept=".jpg, .png, .jpeg, .gif, .bmp, .bmp, .tiff|image/*" required>
+                                                    <label class="custom-file-label" for="customFileUpload"><?php echo e(\App\CPU\translate('Upload')); ?> <?php echo e(\App\CPU\translate('image')); ?></label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-md-8 mb-3 mb-sm-0 mt-0">
+                                            <label for="description">আপনার প্রকাশনী সম্পর্কে কিছু বলুন</label><br/>
+                                            <textarea name="description" id="description" style="width: 100%; height: 220px;" placeholder="আপনার প্রকাশনী সম্পর্কে কিছু বলুন" required></textarea>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <label for="collection_point">বই সংগ্রহ করার পয়েন্ট</label>
+                                                    <select name="collection_point" id="collection_point" class="form-control form-control-user">
+                                                        <option selected disabled>নির্বাচন করুন</option>
+                                                        <option value="বাংলাবাজার">বাংলাবাজার</option>
+                                                        <option value="কাঁটাবন">কাঁটাবন</option>
+                                                        <option value="মিরপুর (হেড অফিস)">মিরপুর (হেড অফিস)</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col-md-12 mt-3">
+                                                    <label for="payment_number">পেমেন্ট গ্রহণ করার জন্য আপনার নম্বরটি দিন</label>
+                                                    <input type="number" class="form-control form-control-user" id="payment_number" name="payment_number" value="<?php echo e(old('payment_number')); ?>" placeholder="01712345678" maxlength="11" required>
+                                                </div>
+                                                <div class="col-md-12 mt-3">
+                                                    <label for="payment_option">আপনার নম্বরটি কোন সেবার আওতাধীন?</label>
+                                                    <select name="payment_option" id="payment_option" class="form-control form-control-user">
+                                                        <option selected disabled>নির্বাচন করুন</option>
+                                                        <option value="বিকাশ">বিকাশ</option>
+                                                        <option value="নগদ">নগদ</option>
+                                                        <option value="রকেট">মিরপুর (হেড অফিস)</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-md-4 mb-3 mb-sm-0">
+                                            <label for="email">আপনার ই-মেইল</label>
+                                            <input type="email" class="form-control form-control-user" id="email" name="email" value="<?php echo e(old('email')); ?>" placeholder="bdjp@booksbd.net" required>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="password">আপনার পাসওয়ার্ড দিন</label>
+                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="পাসওয়ার্ড লিখুন" required>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="password_confirmation ">আপনার পাসওয়ার্ডটি পুনরায় দিন</label>
+                                            <input type="password" class="form-control form-control-user" id="password_confirmation" name="password_confirmation" placeholder="পুনরায় পাসওয়ার্ড লিখুন" required>
+                                        </div>
+                                    </div>
+                                    <div class="">
+                                        <div class="pb-1 mb-2">
+                                            <center>
+                                                <img style="width: auto;border: 1px solid; max-height:200px;" id="viewer"
+                                                    src="<?php echo e(asset('public\assets\back-end\img\400x400\img2.jpg')); ?>" alt="banner image"/>
+                                            </center>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block" id="apply">দাখিল করুন</button>
+                                </form>
+                                <hr>
+                                <div class="text-center mt-4">
+                                    আমাদের সাথে একাউন্ট আছে?  <a href="<?php echo e(route('seller.auth.login')); ?>" style="color: #339B38 !important;">সাইন ইন</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
 
             </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header text-capitalize">
-                        <?php echo e(\App\CPU\translate('Seller')); ?> <?php echo e(\App\CPU\translate('Account')); ?> <br>
-                        <?php if($seller->status=='approved'): ?>
-                            <form class="d-inline-block" action="<?php echo e(route('admin.sellers.updateStatus')); ?>" method="POST">
-                                <?php echo csrf_field(); ?>
-                                <input type="hidden" name="id" value="<?php echo e($seller->id); ?>">
-                                <input type="hidden" name="status" value="suspended">
-                                <button type="submit"
-                                        class="btn btn-outline-danger"><?php echo e(\App\CPU\translate('suspend')); ?></button>
-                            </form>
-                        <?php elseif($seller->status=='rejected' || $seller->status=='suspended'): ?>
-                            <form class="d-inline-block" action="<?php echo e(route('admin.sellers.updateStatus')); ?>" method="POST">
-                                <?php echo csrf_field(); ?>
-                                <input type="hidden" name="id" value="<?php echo e($seller->id); ?>">
-                                <input type="hidden" name="status" value="approved">
-                                <button type="submit"
-                                        class="btn btn-outline-success"><?php echo e(\App\CPU\translate('activate')); ?></button>
-                            </form>
-                        <?php endif; ?>
-                    </div>
-                    <div class="card-body">
-                        <div class="card-body" style="text-align: <?php echo e(Session::get('direction') === "rtl" ? 'right' : 'left'); ?>;">
-                            <div class="flex-start">
-                                <div><h4>Status : </h4></div>
-                                <div class="mx-1"><h4><?php echo $seller->status=='approved'?'<label class="badge badge-success">Active</label>':'<label class="badge badge-danger">In-Active</label>'; ?></h4></div>
-                            </div>
-                            <div class="flex-start">
-                                <div><h5><?php echo e(\App\CPU\translate('name')); ?> : </h5></div>
-                                <div class="mx-1"><h5><?php echo e($seller->f_name); ?> <?php echo e($seller->l_name); ?></h5></div>
-                            </div>
-                            <div class="flex-start">
-                                <div><h5><?php echo e(\App\CPU\translate('Email')); ?> : </h5></div>
-                                <div class="mx-1"><h5><?php echo e($seller->email); ?></h5></div>
-                            </div>
-                            <div class="flex-start">
-                                <div><h5><?php echo e(\App\CPU\translate('Phone')); ?> : </h5></div>
-                                <div class="mx-1"><h5><?php echo e($seller->phone); ?></h5></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <?php if($seller->shop): ?>
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <?php echo e(\App\CPU\translate('Shop')); ?> <?php echo e(\App\CPU\translate('info')); ?>
-
-                        </div>
-                        <div class="card-body" style="text-align: <?php echo e(Session::get('direction') === "rtl" ? 'right' : 'left'); ?>;">
-                            <div class="flex-start">
-                                <div><h5><?php echo e(\App\CPU\translate('seller')); ?> : </h5></div>
-                                <div class="mx-1"><h5><?php echo e($seller->shop->name); ?></h5></div>
-                            </div>
-                            <div class="flex-start">
-                                <div><h5><?php echo e(\App\CPU\translate('Phone')); ?> : </h5></div>
-                                <div class="mx-1"><h5><?php echo e($seller->shop->contact); ?></h5></div>
-                            </div>
-                            <div class="flex-start">
-                                <div><h5><?php echo e(\App\CPU\translate('address')); ?> : </h5></div>
-                                <div class="mx-1"><h5><?php echo e($seller->shop->address); ?></h5></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            <?php endif; ?>
-
-            <div class="col-md-6 mt-3">
-                <div class="card">
-                    <div class="card-header">
-                        <?php echo e(\App\CPU\translate('bank_info')); ?>
-
-                    </div>
-                    <div class="card-body" style="text-align: <?php echo e(Session::get('direction') === "rtl" ? 'right' : 'left'); ?>;">
-                        <div class="col-md-8 mt-2">
-                            <div class="flex-start">
-                                <div><h4><?php echo e(\App\CPU\translate('bank_name')); ?> : </h4></div>
-                                <div class="mx-1"><h4><?php echo e($seller->bank_name ? $seller->bank_name : 'No Data found'); ?></h4></div>
-                            </div>
-                            <div class="flex-start">
-                                <div><h6><?php echo e(\App\CPU\translate('Branch')); ?> : </h6></div>
-                                <div class="mx-1"><h6><?php echo e($seller->branch ? $seller->branch : 'No Data found'); ?></h6></div>
-                            </div>
-                            <div class="flex-start">
-                                <div><h6><?php echo e(\App\CPU\translate('holder_name')); ?> : </h6></div>
-                                <div class="mx-1"><h6><?php echo e($seller->holder_name ? $seller->holder_name : 'No Data found'); ?></h6></div>
-                            </div>
-                            <div class="flex-start">
-                                <div><h6><?php echo e(\App\CPU\translate('account_no')); ?> : </h6></div>
-                                <div class="mx-1"><h6><?php echo e($seller->account_no ? $seller->account_no : 'No Data found'); ?></h6></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            
         </div>
 </div>
 <?php $__env->stopSection(); ?>
