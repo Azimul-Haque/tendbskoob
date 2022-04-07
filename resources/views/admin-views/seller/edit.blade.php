c946dd417ba2fb4987fb25bb37e2cafd18b646a3
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-07 10:29:22 +0600

    Commit Thu 04/07/2022 10:29:22.39 27405

--- resources/views/admin-views/seller/edit.blade.php
+++ resources/views/admin-views/seller/edit.blade.php
@@ -55,42 +55,7 @@
                                class="tio-edit"></i>{{\App\CPU\translate('Seller_Edit')}}</h4>
                     </div>
                     <div class="row gx-2 gx-lg-3" id="order_stats">
-                        <div class="flex-between" style="width: 100%">
-                            <div class="mb-3 mb-lg-0" style="width: 18%">
-                                <div class="card card-body card-hover-shadow h-100 text-white text-center" style="background-color: #22577A;">
-                                    <h1 class="p-2 text-white">{{ $seller->wallet ? \App\CPU\BackEndHelper::set_symbol(\App\CPU\BackEndHelper::usd_to_currency($seller->wallet->commission_given)) : 0}}</h1>
-                                    <div class="text-uppercase">{{\App\CPU\translate('commission_given')}}</div>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 mb-lg-0" style="width: 18%">
-                                <div class="card card-body card-hover-shadow h-100 text-white text-center" style="background-color: #595260;">
-                                    <h1 class="p-2 text-white">{{ $seller->wallet ? \App\CPU\BackEndHelper::set_symbol(\App\CPU\BackEndHelper::usd_to_currency($seller->wallet->pending_withdraw)) : 0}}</h1>
-                                    <div class="text-uppercase">{{\App\CPU\translate('pending_withdraw')}}</div>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 mb-lg-0" style="width: 18%">
-                                <div class="card card-body card-hover-shadow h-100 text-white text-center" style="background-color: #a66f2e;">
-                                    <h1 class="p-2 text-white">{{ $seller->wallet ? \App\CPU\BackEndHelper::set_symbol(\App\CPU\BackEndHelper::usd_to_currency($seller->wallet->delivery_charge_earned)) : 0}}</h1>
-                                    <div class="text-uppercase">{{\App\CPU\translate('delivery_charge_earned')}}</div>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 mb-lg-0" style="width: 18%">
-                                <div class="card card-body card-hover-shadow h-100 text-white text-center" style="background-color: #6E85B2;">
-                                    <h1 class="p-2 text-white">{{ $seller->wallet ? \App\CPU\BackEndHelper::set_symbol(\App\CPU\BackEndHelper::usd_to_currency($seller->wallet->collected_cash)) : 0}}</h1>
-                                    <div class="text-uppercase">{{\App\CPU\translate('collected_cash')}}</div>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 mb-lg-0" style="width: 18%">
-                                <div class="card card-body card-hover-shadow h-100 text-white text-center" style="background-color: #6D9886;">
-                                    <h1 class="p-2 text-white">{{ $seller->wallet ? \App\CPU\BackEndHelper::set_symbol(\App\CPU\BackEndHelper::usd_to_currency($seller->wallet->total_tax_collected)) : 0}}</h1>
-                                    <div class="text-uppercase">{{\App\CPU\translate('total_collected_tax')}}</div>
-                                </div>
-                            </div>
-                        </div>
+                        s
                     </div>
                     <div class="row">
                     <!-- Earnings (Monthly) Card Example -->
