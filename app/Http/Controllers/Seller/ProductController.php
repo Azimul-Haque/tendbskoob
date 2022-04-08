0ba67145e3091d3f49ff0a7b9818e8dc2e0c3848
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-08 13:36:26 +0600

    Commit Fri 04/08/2022 13:36:26.36 4721

--- app/Http/Controllers/Seller/ProductController.php
+++ app/Http/Controllers/Seller/ProductController.php
@@ -171,9 +171,7 @@ class ProductController extends Controller
 
         // $p->video_provider = 'youtube';
         // $p->video_url = $request->video_link;
-        if(auth('admin')->user()->role->name != 'Master Admin' && auth('admin')->user()->role->name != 'Admin') {
-            $p->status = 0;
-        }
+        $p->status = 0;
         $p->request_status = 1; // status default to 1
         if($p->current_stock > 0) {
             $p->stock_status = $request->stock_status; // 1 = in stock, 2 = out of stock, 3 = back order
