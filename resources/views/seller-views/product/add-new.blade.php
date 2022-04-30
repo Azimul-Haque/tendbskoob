2ddc366ed55f4c6a366e3f620173653d0081aae9
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-30 21:20:07 +0600

    Commit Sat 04/30/2022 21:20:07.40 3044

--- resources/views/seller-views/product/add-new.blade.php
+++ resources/views/seller-views/product/add-new.blade.php
@@ -330,9 +330,9 @@
                                             <label class="control-label">কাস্টমার কমিশন (%) <small id="unit_percentage_text"></small></label>
                                             <input type="number" min="0" step="0.01" max="100"
                                                 placeholder="শুধুমাত্র ইংরেজি নম্বরে পারসেন্টিজটি উল্লেখ করুন"
-                                                name="unit_price" 
-                                                onkeyup="purchasePercetage()"
-                                                id="unit_price" value="{{old('unit_price')}}" class="form-control"
+                                                value="{{old('unit_price')}}"
+                                                onkeyup="unitPercetage()"
+                                                name="unit_price" id="unit_price"  class="form-control"
                                                 required>
                                         </div>
                                     @endif
