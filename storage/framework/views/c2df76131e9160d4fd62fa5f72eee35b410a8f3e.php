910a79011caf4863da68b9faef126dbcf7d552a3
Azimul-Haque <orbachinujbuk@gmail.com>
2022-05-01 03:43:45 +0600

    Commit Sun 05/01/2022  3:43:45.37 874

--- storage/framework/views/c2df76131e9160d4fd62fa5f72eee35b410a8f3e.php
+++ storage/framework/views/c2df76131e9160d4fd62fa5f72eee35b410a8f3e.php
@@ -35,7 +35,7 @@
                                             <div class="product-title">
                                                 <a href="<?php echo e(route('product',$cartItem['slug'])); ?>"><?php echo e($cartItem['name_bangla']); ?></a>
                                                 <?php if($cartItem['preorder_status'] == 1): ?>
-                                                    <label style="background-color: #FF9900 !important; color: #FFFFFF !important;" class="badge badge-danger stock-out">Pre Order</label><br/><br/>
+                                                    <span style="background-color: #FF9900 !important; color: #FFFFFF !important;" class="badge badge-danger stock-out">Pre Order</span><br/><br/>
                                                 <?php endif; ?>
                                                 <br/>
                                                 <small><?php echo e($cartItem['author']); ?></small>
