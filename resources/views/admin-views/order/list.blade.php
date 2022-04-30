1f4c63c30293f1c590638cee4225606c713dbf32
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-30 22:31:22 +0600

    Commit Sat 04/30/2022 22:31:22.40 23948

--- resources/views/admin-views/order/list.blade.php
+++ resources/views/admin-views/order/list.blade.php
@@ -185,6 +185,14 @@
                                         <a class="dropdown-item" target="_blank"
                                            href="{{route('admin.orders.generate-invoice',[$order['id']])}}"><i
                                                 class="tio-delete"></i> Delete</a>
+                                                <a class="btn btn-danger btn-sm" href="javascript:"
+                                                onclick="form_alert('product-{{$p['id']}}','Want to delete this item ?')">
+                                                    <i class="tio-add-to-trash"></i> {{\App\CPU\translate('Delete')}}
+                                                </a>
+                                                <form action="{{route('admin.product.delete',[$p['id']])}}"
+                                                    method="post" id="product-{{$p['id']}}">
+                                                    @csrf @method('delete')
+                                                </form>
                                     </div>
                                 </div>
                             </td>
