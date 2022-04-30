5a2f2203605861f481a102e9ef339b50b3d9e3a5
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-30 21:18:22 +0600

    Commit Sat 04/30/2022 21:18:22.41 21466

--- storage/framework/views/4596e583ab6fe3723e1971b174cc3050ca363d43.php
+++ storage/framework/views/4596e583ab6fe3723e1971b174cc3050ca363d43.php
@@ -153,23 +153,24 @@
                                             <label class="control-label">মুদ্রিত মূল্য (৳)</label>
                                             <input type="number" min="0" step="0.01"
                                                 placeholder="<?php echo e(\App\CPU\translate('Published Price')); ?>"
-                                                name="published_price" value="<?php echo e(old('published_price')); ?>" class="form-control"
+                                                name="published_price" id="published_price" value="<?php echo e(old('published_price')); ?>" class="form-control"
                                                 required>
                                         </div>    
                                         <div class="col-md-4">
                                             <label
-                                                class="control-label">বুকসবিডির কমিশন (৳)</label>
+                                                class="control-label">বুকসবিডির কমিশন (%) <small>asd</small></label>
                                             <input type="number" min="0" step="0.01" max="100"
                                                 placeholder="শুধুমাত্র ইংরেজি নম্বরে পারসেন্টিজটি উল্লেখ করুন"
                                                 value="<?php echo e(old('purchase_price')); ?>"
-                                                name="purchase_price" class="form-control" required>
+                                                onkeyup="purchasePercetage()"
+                                                name="purchase_price" id="purchase_price" class="form-control" required>
                                         </div>
                                         
                                         <div class="col-md-4">
-                                            <label class="control-label">কাস্টমার কমিশন (৳)</label>
+                                            <label class="control-label">কাস্টমার কমিশন (%)</label>
                                             <input type="number" min="0" step="0.01" max="100"
                                                 placeholder="শুধুমাত্র ইংরেজি নম্বরে পারসেন্টিজটি উল্লেখ করুন"
-                                                name="unit_price" value="<?php echo e(old('unit_price')); ?>" class="form-control"
+                                                name="unit_price" id="unit_price" value="<?php echo e(old('unit_price')); ?>" class="form-control"
                                                 required>
                                         </div>
                                     <?php endif; ?>
