43ca2ac8bef57bdcbc96b95f602ac7f346d0c14a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-05-01 01:47:47 +0600

    Commit Sun 05/01/2022  1:47:47.36 10432

--- storage/framework/views/6ce1557a866fcce1b9ec491aabde27dd2806992f.php
+++ storage/framework/views/6ce1557a866fcce1b9ec491aabde27dd2806992f.php
@@ -263,6 +263,19 @@
                                 <?php if($product['current_stock'] > 0): ?>
                                     <h5 class="mt-3" style="color: green"><img style="margin-right: 8px; width: 18px; height: auto;" src="<?php echo e(asset('public/assets/front-end/img/in-stock(mini).svg')); ?>"> <?php echo e(\App\CPU\translate('Book in Stock')); ?></h5>
                                 <?php endif; ?>
+                                <?php
+                                    $category_names = [];
+                                    if($product->categories->count() > 0) {
+                                        for($i = 0; $i < count($product->categories); $i++){
+                                            $category_names[] = $product->categories[$i]->name;
+                                        }
+                                    }
+                                ?>
+                                <?php if(in_array('Pre Order', $category_names)): ?>
+                                    <label style="left: 29%!important; top: 29%!important; background-color: #FF9900 !important; color: #FFFFFF !important;"
+                                           class="badge badge-danger stock-out">Pre Order</label>
+                                    
+                                <?php endif; ?>
                             </div>
                         </div>
 
