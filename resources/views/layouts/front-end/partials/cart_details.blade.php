4fad03a207451bc11c6e9bb9d083b4aa7bde138f
Azimul-Haque <orbachinujbuk@gmail.com>
2022-05-01 03:43:18 +0600

    Commit Sun 05/01/2022  3:43:18.36 6267

--- resources/views/layouts/front-end/partials/cart_details.blade.php
+++ resources/views/layouts/front-end/partials/cart_details.blade.php
@@ -41,7 +41,6 @@
                                                 @if($cartItem['preorder_status'] == 1)
                                                     <label style="background-color: #FF9900 !important; color: #FFFFFF !important;" class="badge badge-danger stock-out">Pre Order</label><br/><br/>
                                                 @endif
-                                                {{ $cartItem['preorder_status'] }}
                                                 <br/>
                                                 <small>{{$cartItem['author']}}</small>
                                             </div>
