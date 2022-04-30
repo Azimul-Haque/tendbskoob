05994fb94cf38c2168360106a33e52a7c9dc893a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-04-30 21:08:49 +0600

    Commit Sat 04/30/2022 21:08:49.39 9070

--- resources/views/admin-views/product/add-new.blade.php
+++ resources/views/admin-views/product/add-new.blade.php
@@ -307,6 +307,14 @@
                                 </div>
                                 <div class="row pt-4">
                                     @if (auth('admin')->user()->role->name == 'Master Admin' || auth('admin')->user()->role->name == 'Admin')
+                                        
+                                        <div class="col-md-4">
+                                            <label class="control-label">{{\App\CPU\translate('Published Price')}} (৳)</label>
+                                            <input type="number" min="0" step="0.01"
+                                                placeholder="{{\App\CPU\translate('Published Price')}}"
+                                                name="published_price" value="{{old('published_price')}}" class="form-control"
+                                                required>
+                                        </div>
                                         <div class="col-md-4">
                                             <label
                                                 class="control-label">{{\App\CPU\translate('Purchase Price')}} (৳)</label>
@@ -315,13 +323,7 @@
                                                 value="{{old('purchase_price')}}"
                                                 name="purchase_price" class="form-control" required>
                                         </div>
-                                        <div class="col-md-4">
-                                            <label class="control-label">{{\App\CPU\translate('Published Price')}} (৳)</label>
-                                            <input type="number" min="0" step="0.01"
-                                                placeholder="{{\App\CPU\translate('Published Price')}}"
-                                                name="published_price" value="{{old('published_price')}}" class="form-control"
-                                                required>
-                                        </div>
+                                        
                                         <div class="col-md-4">
                                             <label class="control-label">{{\App\CPU\translate('Sale Price')}} (৳)</label>
                                             <input type="number" min="0" step="0.01"
