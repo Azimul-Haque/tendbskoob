bcad1d37f2a559e2ab98c4b4a0d242be1ff5f09c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-05-06 22:28:56 +0600

    Commit Fri 05/06/2022 22:28:56.40 13757

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -89,7 +89,7 @@ class ProductController extends BaseController
 
     public function store(Request $request)
     {
-        dd($request->release_date);
+        // dd($request->release_date);
         // dd($request->all());
         $validator = Validator::make($request->all(), [
             'publisher_id' => 'required',
