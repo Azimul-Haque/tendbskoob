8761eda0bb332bd5d8ea478f60c3148d90a637cb
Azimul-Haque <orbachinujbuk@gmail.com>
2022-05-06 22:40:02 +0600

    Commit Fri 05/06/2022 22:40:02.47 16037

--- storage/framework/views/4aba07436fcd09c6b14e3063b16e6a2685fcf658.php
+++ storage/framework/views/4aba07436fcd09c6b14e3063b16e6a2685fcf658.php
@@ -134,7 +134,7 @@
                                                 <span class="badge badge-info">Pending</span> 
                                             <?php endif; ?> <br/>
                                             <?php if(in_array('Pre Order', $category_names) && $p->release_date): ?>
-                                                <small style="color: #0300c4 !important;">Release Date: <?php echo e($p->release_date ? date('F d, Y', strtotime($p->release_date)) : ''); ?></small><br/><br/>
+                                                <small style="color: #0300c4 !important;">Release Date: <?php echo e(date('F d, Y', strtotime($p->release_date))); ?></small><br/><br/>
                                             <?php endif; ?>
                                             
                                         </td>
