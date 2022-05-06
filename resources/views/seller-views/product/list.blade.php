7cf971c39a5f0511e6fdd3f9ffaf29b10f7a5df6
Azimul-Haque <orbachinujbuk@gmail.com>
2022-05-06 22:38:55 +0600

    Commit Fri 05/06/2022 22:38:55.42 15430

--- resources/views/seller-views/product/list.blade.php
+++ resources/views/seller-views/product/list.blade.php
@@ -136,7 +136,7 @@
                                                 <span class="badge badge-info">Pending</span> 
                                             @endif <br/>
                                             @if(in_array('Pre Order', $category_names))
-                                                <small style="color: #0300c4 !important;">Release Date: {{ date('F d, Y', strtotime($p->request_status)) }}</small><br/><br/>
+                                                <small style="color: #0300c4 !important;">Release Date: {{ date('F d, Y', strtotime($p->release_date)) }}</small><br/><br/>
                                             @endif
                                             {{-- <label class="switch switch-status">
                                                 <input type="checkbox" class="status"
