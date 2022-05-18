02b20a5dd853a1baea613e8c6f9626f3cfb507f9
Azimul-Haque <orbachinujbuk@gmail.com>
2022-05-19 00:31:14 +0600

    Commit Thu 05/19/2022  0:31:14.42 18358

--- storage/framework/views/4d991c023c1f17710fbe7dd1bfeacc4ce5765f3f.php
+++ storage/framework/views/4d991c023c1f17710fbe7dd1bfeacc4ce5765f3f.php
@@ -186,7 +186,7 @@
                                             class="control-label">বুকসবিডির কমিশন (%) <small id="purchase_percentage_text" style="color: green; font-weight: bold;">৳ <?php echo e($product->purchase_price); ?></small></label>
                                         <input type="number" min="0" step="0.01" max="100"
                                             placeholder="শুধুমাত্র ইংরেজি নম্বরে পারসেন্টিজটি উল্লেখ করুন"
-                                            value="<?php echo e((($product->published_price - $product->purchase_price) / $product->published_price) * 100); ?>"
+                                            value="<?php echo e((($product->published_price - $product->purchase_price) / ($product->published_price == 0 ? 1 : $product->published_price)) * 100); ?>"
                                             onkeyup="purchasePercetage();"
                                             id="purchase_price_percentage" class="form-control" required>
                                         <input type="hidden" name="purchase_price" id="purchase_price">
@@ -196,7 +196,7 @@
                                         <label class="control-label">কাস্টমার কমিশন (%) <small id="unit_percentage_text" style="color: green; font-weight: bold;">৳ <?php echo e($product->unit_price); ?></small></label>
                                         <input type="number" min="0" step="0.01" max="100"
                                             placeholder="শুধুমাত্র ইংরেজি নম্বরে পারসেন্টিজটি উল্লেখ করুন"
-                                            value="<?php echo e((($product->published_price - $product->unit_price) / $product->published_price) * 100); ?>"
+                                            value="<?php echo e((($product->published_price - $product->unit_price) / ($product->published_price == 0 ? 1 : $product->published_price)) * 100); ?>"
                                             onkeyup="unitPercetage()"
                                             id="unit_price_percentage"  class="form-control"
                                             required>
