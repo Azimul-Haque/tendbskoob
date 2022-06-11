dcf4b90256053355eeeb188d7fb3cc2ad2ace2cc
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-12 03:38:59 +0600

    Commit Sun 06/12/2022  3:38:59.38 15782

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -583,11 +583,12 @@ class ProductController extends BaseController
         $p->details       = $request->description;
 
         $p->request_status = 1; // status default to 1
-        if($p->current_stock > 0) {
-            $p->stock_status = $request->stock_status; // 1 = in stock, 2 = out of stock, 3 = back order
-        } else {
-            $p->stock_status = 2; // 1 = in stock, 2 = out of stock, 3 = back order
-        }
+        // if($p->current_stock > 0) {
+        //     $p->stock_status = $request->stock_status; // 1 = in stock, 2 = out of stock, 3 = back order
+        // } else {
+        //     $p->stock_status = 2; // 1 = in stock, 2 = out of stock, 3 = back order
+        // }
+        $p->stock_status = $request->stock_status;;
 
         // release date
         // release date
