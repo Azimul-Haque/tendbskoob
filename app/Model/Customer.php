738c2cbff317c4ddb5bd7dfefaa4b12ca31f3b25
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-12 04:07:50 +0600

    Commit Sun 06/12/2022  4:07:50.33 26056

--- app/Model/Customer.php
+++ app/Model/Customer.php
@@ -13,6 +13,6 @@ class Customer extends Authenticatable
 
     public function shippingAddress()
     {
-        return $this->belongsTo(ShippingAddress::class);
+        return $this->belongsTo(ShippingAddress::class, 'shipping_address');
     }
 }
